9aaf5300cc6da2c33fd41541754562a8283d9d50
Jonas Kupreščenkovas <dzionis1997@gmail.com>
2019-06-21 20:37:12 +0300

    Add project files.

---
+++ WPF chatting app/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WPF_chatting_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Socket sck;
+        EndPoint epLocal, epRemote;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            sck.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            string MyIP = GetLocalIP();
+            string FriendIP = GetLocalIP();
+                
+        }
+
+        private string GetLocalIP()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            return "localhost";
+        }
+
+        private void MessageCallBack(IAsyncResult aResult)
+        {//hello
+            try
+            {
+                int size = sck.EndReceiveFrom(aResult, ref epRemote);
+
+                if (size > 0)
+                {
+                    byte[] receivedData = new byte[1464];
+
+                    receivedData = (byte[])aResult.AsyncState;
+
+                    ASCIIEncoding eEncoding = new ASCIIEncoding();
+                    string receiveMessage = eEncoding.GetString(receivedData);
+
+                    The_chat.Items.Add("Friend : " + receiveMessage);
+                }
+
+                byte[] bufer = new byte[1500];
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("asdasdas");
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void The_chat_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+
+        }
+    }
+}
